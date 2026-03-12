fcf712d58143002b9dee6286f0ee2f867ca72905
FajeSu <fajesu@ya.ru>
2026-03-12 16:45:58 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+def requirements_file_to_list(fn='requirements.txt'):
+    with open(fn, 'r') as f:
+        return [x.rstrip() for x in list(f) if x and not x.startswith('#')]
+
+
+setup(
+    name='m3u82mp3',
+    version='1.0',
+    packages=[
+        'm3u82mp3',
+    ],
+    python_requires='>=3.7, <4',
+    install_requires=requirements_file_to_list(),
+    keywords=['m3u82mp3', 'm3u8-to-mp3'],
+)
