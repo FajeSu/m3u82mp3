ccbf217a6f79be161fb4772d7138ae15050b36e1
FajeSu <fajesu@ya.ru>
2026-03-12 16:52:27 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,6 @@ def requirements_file_to_list(fn='requirements.txt'):
 setup(
     name='m3u82mp3',
     version='1.0',
-    packages=[
-        'm3u82mp3',
-    ],
     python_requires='>=3.7, <4',
     install_requires=requirements_file_to_list(),
     keywords=['m3u82mp3', 'm3u8-to-mp3'],
